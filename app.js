3d90a5b73a2ec6ee99194feef2a9a762e4795adb
kabichandani <karanabichandani13999@yahoo.com>
2020-12-03 00:47:22 -0500

    Finished TestCollection, Add/Delete function working, Working on Poolmapping

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
-const { response } = require('express')
 
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
 
 function getConnection() {
     return mysql.createConnection({
@@ -24,7 +25,7 @@ app
         labid = req.body.labid
         password = req.body.password
         getConnection().query(`SELECT * FROM finalproject WHERE labid = '${labid}' AND password = '${password}'`, (err,results) => {
-            console.log(results)
+            if (err) throw err;
             if (results.length > 0){
                 res.redirect("/labhome")
                 res.end()
@@ -39,13 +40,65 @@ app.get('/labhome', function(req, res) {
     res.sendFile('./LabHome.html',{root: __dirname })
 });
 
-app.get('/labhome/testcollection', function(req, res) {
-    res.sendFile('./TestCollection.html',{root: __dirname })
-});
+app
+    .route("/labhome/testcollection")
+    .get((req,res) => {
+        res.sendFile('./TestCollection.html',{root: __dirname })
+    })
+    .post((req,res) => {
+        action = req.body.event
+        empid = req.body.empid
+        barcode = req.body.barcode
+        if (action == "Add"){
+            if (empid == "" || barcode == ""){
+                getConnection().query(`SELECT empid, barcode FROM finalproject`, (err,results) =>{
+                    if (err) throw err;
+                    res.json(results)
+                })
+            }
+            else{
+                getConnection().query(`SELECT * FROM finalproject WHERE empid = '${empid}' AND password = '${barcode}'`, (err,results) => {
+                    if (err) throw err;
+                    if (results.length == 0){
+                        getConnection().query(`INSERT INTO finalproject (empid,barcode) VALUES (${empid},${barcode})`, (err,results1) =>{
+                            if (err) throw err
+                            getConnection().query(`SELECT empid, barcode FROM finalproject`, (err,results) =>{
+                                if (err) throw err;
+                                res.json(results)
+                            })
+                        })                        
+                    }
+                })
+            }
+            
+        }
+        if (action == "Delete"){
+            checkedValues = req.body.array
+            var resultsStore = [];
+            for (var i = 0; i < checkedValues.length; i++){
+                getConnection().query(`SELECT * FROM finalproject LIMIT ${checkedValues[i]},1`, (err,results) =>{
+                    if (err) throw err
+                    getConnection().query(`DELETE FROM finalproject WHERE empid = ${results[0].empid} AND barcode = ${results[0].barcode}`, (err,results1 =>{
+                        if (err) throw err;
+                        getConnection().query(`SELECT empid, barcode FROM finalproject`, (err,results2) =>{
+                            if (err) throw err;
+                        })
+                    }))
+                })
+            }
+            res.json(resultsStore)            
+        }
+    })
 
-app.get('/labhome/poolmapping', function(req, res) {
-    res.sendFile('./PoolMapping.html',{root: __dirname })
-});
+app
+    .route('/labhome/poolmapping')
+    .get((req, res) => {
+        res.sendFile('./PoolMapping.html',{root: __dirname })
+    })
+    .post((req,res) =>{
+        
+    })
+
 
 app.get('/labhome/welltesting', function(req, res) {
     res.sendFile('./WellTesting.html',{root: __dirname })
