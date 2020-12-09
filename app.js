359ea95dbcc679237c4bed0b777354887f6e054f
kabichandani <karanabichandani13999@yahoo.com>
2020-12-09 01:31:19 -0500

    Almost done with poolmapping.html. Worked everything else just need to fix backend edit pool button

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const moment = require('moment')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
@@ -12,13 +13,16 @@ function getConnection() {
         host:'localhost',
         user: 'root',
         password: 'pass4root',
-        database: 'finalProject316'
+        database: 'finalproject316'
     })
 }
+
 
 app.get('/', function(req, res){
     res.redirect('/labtech');
 });
+
+var labid_global;
 
 app
     .route('/labtech')
@@ -27,6 +31,7 @@ app
     })
     .post((req,res) => {
         labid = req.body.labid
+        labid_global = labid
         password = req.body.password
         getConnection().query(`SELECT * FROM LabEmployee WHERE labID = '${labid}' AND password = '${password}'`, (err,results) => {
             if (err) throw err;
@@ -55,16 +60,17 @@ app
         barcode = req.body.barcode
         if (action == "Add"){
             if (empid == "" || barcode == ""){
-                getConnection().query(`SELECT employeeID, testBarcode FROM employeetest`, (err,results) =>{
+                getConnection().query(`SELECT employeeID, testBarcode FROM employeetest WHERE collectedBy = ${labid_global}`, (err,results) =>{
                     if (err) throw err;
                     res.json(results)
                 })
             }
             else{
-                getConnection().query(`SELECT * FROM employeetest WHERE employeeID = '${empid}' AND testBarcode = '${barcode}'`, (err,results) => {
+                getConnection().query(`SELECT * FROM employeetest WHERE employeeID = '${empid}' AND testBarcode = '${barcode}' AND collectedBy = ${labid_global}`, (err,results) => {
                     if (err) throw err;
                     if (results.length == 0){
-                        getConnection().query(`INSERT INTO employeetest (employeeID,testBarcode) VALUES (${empid},${barcode})`, (err,results1) =>{
+                        var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+                        getConnection().query(`INSERT INTO employeetest (employeeID,testBarcode,collectionTime,collectedBy) VALUES ('${empid}','${barcode}','${mysqlTimestamp}','${labid_global}')`, (err,results1) =>{
                             if (err) throw err
                             getConnection().query(`SELECT employeeID, testBarcode FROM employeetest`, (err,results) =>{
                                 if (err) throw err;
@@ -74,16 +80,14 @@ app
                     }
                 })
             }
-            
         }
         if (action == "Delete"){
             checkedValues = req.body.array
             var resultsStore = [];
             for (var i = 0; i < checkedValues.length; i++){
                 getConnection().query(`SELECT * FROM employeetest LIMIT ${checkedValues[i]},1`, (err,results) =>{
-                    console.log(results)
                     if (err) throw err
-                    getConnection().query(`DELETE FROM employeetest WHERE employeeID = ${results[0].employeeID} AND testBarcode = ${results[0].testBarcode}`, (err,results1 =>{
+                    getConnection().query(`DELETE FROM employeetest WHERE employeeID = ${results[0].employeeID} AND testBarcode = ${results[0].testBarcode} AND collectedBy = ${labid_global}`, (err,results1 =>{
                         if (err) throw err;
                         getConnection().query(`SELECT employeeID, testBarcode FROM employeetest`, (err,results2) =>{
                             if (err) throw err;
@@ -100,7 +104,7 @@ app
     .get((req, res) => {
         res.sendFile('./PoolMapping.html',{root: __dirname })
     })
-    .post((req,res) =>{
+    .post((req,res) => {
         barcodes = req.body.barcodes
         poolBarcode = req.body.poolBarcode
         action = req.body.action
@@ -112,30 +116,40 @@ app
                 })
             }
             else{
-                getConnection().query(`SELECT * FROM Pool WHERE poolBarcode = '${poolBarcode}'`, (err,results) => {
+                getConnection().query(`SELECT * FROM pool WHERE poolBarcode = '${poolBarcode}'`, (err,results) => {
                     if (err) throw err;
                     if (results.length == 0){
-                        getConnection().query(`INSERT INTO Pool (poolBarcode) VALUES (${poolBarcode})`, (err,results1) => {
-                            console.log(barcodes)
+                        getConnection().query(`INSERT INTO Pool (poolBarcode) VALUES ('${poolBarcode}')`, (err,results1) => {
                             if (err) throw err;
                             for (var i = 0; i < barcodes.length; i++){
-                                if (barcodes[i]){
-                                    getConnection().query(`INSERT INTO poolmap (testBarcode,poolBarcode) VALUES (${barcodes[i]},${poolBarcode})`, (err,results1) =>{
+                                if (i == barcodes.length - 1){
+                                    getConnection().query(`INSERT INTO poolmap (testBarcode,poolBarcode) VALUES (${barcodes[i]},${poolBarcode})`, (err,results2) =>{
+                                        if (err) throw err
+                                        getConnection().query(`SELECT poolBarcode, testBarcode FROM poolmap`, (err,results) =>{
+                                            if (err) throw err
+                                            res.json(results)
+                                        })  
+                                    })
+                                }
+                                else{
+                                    getConnection().query(`INSERT INTO poolmap (testBarcode,poolBarcode) VALUES (${barcodes[i]},${poolBarcode})`, (err,results2) =>{
                                         if (err) throw err
                                     })        
-                                }  
+                                }
                             }   
-                            getConnection().query(`SELECT empid, barcode FROM finalproject`, (err,results) =>{
-                                if (err) throw err;
-                                res.json(results)
-                            })          
                         })
                     }
                 })
             }
         }
         if (action == "Edit Pool"){
-
+            
+        }
+        if (action == "Show"){
+            getConnection().query(`SELECT poolBarcode, testBarcode FROM poolmap`, (err,results) =>{
+                if (err) throw err;
+                res.json(results)
+            })   
         }
     })
 
@@ -144,9 +158,26 @@ app.get('/labtech/labhome/welltesting', function(req, res) {
     res.sendFile('./WellTesting.html',{root: __dirname })
 });
 
-app.get('/employee', function(req, res) {
-    res.sendFile('./EmployeeLogin.html',{root: __dirname })
-});
+app
+    .route('/employee')
+    .get((req, res) => {
+        res.sendFile('./EmployeeLogin.html',{root: __dirname })
+    })
+    .post((req,res) => {
+        email = req.body.email
+        password = req.body.password
+        res.sendFile('./EmployeeLogin.html',{root: __dirname })
+        getConnection().query(`SELECT * FROM Employee WHERE email = '${email}' AND passcode = '${password}'`, (err,results) => {
+            if (err) throw err;
+            if (results.length > 0){
+                res.redirect("/employee")
+                res.end()
+            }
+            else{
+                res.sendFile('./labtech.html',{root: __dirname })
+            }
+        })
+    })
 
 app.listen('3000', err =>{
     if (err) throw err
